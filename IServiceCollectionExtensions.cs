1a8c8568b50e2a633356a1fe1a940cd45355f6f3
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2025-11-20 16:45:53 +0100

    ✨ Create extension method on IHostApplicationBuilder

--- IServiceCollectionExtensions.cs
+++ IServiceCollectionExtensions.cs
@@ -10,6 +10,16 @@ namespace NLogFlake;
 
 public static class IServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds LogFlake logging services and configuration to the specified service collection.
+    /// </summary>
+    /// <remarks>This method registers all required LogFlake services, including configuration, HTTP client,
+    /// and core logging interfaces. It should be called during application startup to enable LogFlake logging
+    /// features.
+    /// Note: this method may be removed in following version, please move to <see cref="IHostApplicationBuilderExtensions.AddLogFlake(Microsoft.Extensions.Hosting.IHostApplicationBuilder)"/></remarks>
+    /// <param name="services">The service collection to which the LogFlake services will be added. Cannot be null.</param>
+    /// <param name="configuration">The application configuration containing the LogFlake settings. Cannot be null.</param>
+    /// <returns>The same service collection instance, for chaining further service registrations.</returns>
     public static IServiceCollection AddLogFlake(this IServiceCollection services, IConfiguration configuration)
     {
         _ = services.Configure<LogFlakeOptions>(configuration.GetSection(LogFlakeOptions.SectionName))
